c3b3d8316e75c45221fb11e6d616cd3cd477fd3c
DESKTOP-M26OLTQ\ITHUT <slovenblack@gmail.com>
2021-01-28 20:26:41 +0700

    Send Assignment Week1

---
+++ week1/Assets/Week1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Week1 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+   
+    
+    public string Win;
+    public GameObject input;
+    public GameObject output;
+
+    public void tao()
+    {
+        Win = input.GetComponent<Text>().text;
+        if (Win == "Unity")
+        {
+            output.GetComponent<Text>().text="[ Unity ] Is Found";
+        }
+        if (Win == "Unreal")
+        {
+            output.GetComponent<Text>().text = "[ Unreal ] Is Found";
+        }
+        if (Win == "Google")
+        {
+            output.GetComponent<Text>().text = "[ Google ] Is Found";
+        }
+        if (Win == "Line")
+        {
+            output.GetComponent<Text>().text = "[ Line ] Is Found";
+        }
+        if (Win == "Facebook")
+        {
+            output.GetComponent<Text>().text = "[ Facebook ] Is Found";
+        }
+        if (Win!= "Unity") 
+        {
+            output.GetComponent<Text>().text = "[         ]Is not Found";
+        }
+        
+       
+    }
+        
+    
+}
